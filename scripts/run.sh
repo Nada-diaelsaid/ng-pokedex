e4e286f4c45829e2f7badc065d50a52abba9592d
nada-diaelsaid <nada_diaelsaid@hotmail.com>
2021-06-26 19:22:10 +0200

    fix exit status

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,11 @@
-#!/bin/bata
+#!/bin/bash
+
+set -o pipefail
 
 echo "On branch ${BRANCH}..."
-OUTPUT_DIR=/artifacts/${BRANCH}/${TIMESTAMP}
+timestamp=$(date +"%Y%m%d%H%M%S")
+OUTPUT_DIR=/artifacts/${BRANCH}/${timestamp}
+exit_status=0
 
 if [ ! -d ${OUTPUT_DIR} ]; then
     mkdir -p ${OUTPUT_DIR}
@@ -9,16 +13,21 @@ fi
 
 if [ $1 == "lint" ]; then
     FILE_NAME=${1}.log
-    ng lint > >(tee -a ${FILE_NAME}) 2> >(tee -a ${FILE_NAME})
+    ng lint |& tee ${FILE_NAME}
+    exit_status=${PIPESTATUS[0]}
 elif [ $1 == "unit" ]; then
     FILE_NAME=${1}.log
-    ng test > >(tee -a ${FILE_NAME}) 2> >(tee -a ${FILE_NAME})
+    ng test |& tee ${FILE_NAME}
+    exit_status=${PIPESTATUS[0]}
 elif [ $1 == "e2e" ]; then
     FILE_NAME=${1}.log
-    ng e2e > >(tee -a ${FILE_NAME}) 2> >(tee -a ${FILE_NAME})
+    ng e2e |& tee ${FILE_NAME}
+    exit_status=${PIPESTATUS[0]}
 elif [ $1 == "dist" ]; then
     FILE_NAME=${1}.zip
     zip -r ${FILE_NAME} ${1}
+    exit_status=${PIPESTATUS[0]}
 fi
 
 mv ${FILE_NAME} ${OUTPUT_DIR}/${FILE_NAME}
+exit ${exit_status}
